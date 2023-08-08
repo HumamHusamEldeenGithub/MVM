34fc509b69ca20118c120a7ea8557e597b08e62f
HumamHusamEldeen <humamhusameldeen.github@gmail.com>
2023-08-08 20:21:40 +0300

    Buttons fixes

---
+++ Assets/Scripts/C#/UI/LoginSignupPanel.cs
@@ -0,0 +1,58 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class LoginSignupPanel : MonoBehaviour
+{
+    #region Attributes
+
+    [SerializeField]
+    private TMP_InputField usernameInp;
+
+    [SerializeField]
+    private TMP_InputField passwordInp;
+
+    [SerializeField]
+    private Button loginBtn;
+
+    [SerializeField]
+    private GameObject errorMsgGO;
+
+    #endregion
+    private void OnEnable()
+    {
+        if (!usernameInp || !passwordInp || !loginBtn )
+        {
+            Debug.LogWarning("Username or Password isn't populated");
+        }
+
+        loginBtn.onClick.AddListener(Login);
+
+        EventsPool.Instance.AddListener(typeof(LoginStatusEvent),
+            new Action<bool>(OnLogin));
+
+    }
+
+    private void Login()
+    {
+        //errorMsgGO.SetActive(false);
+        EventsPool.Instance.InvokeEvent(typeof(SubmitLoginEvent),
+            new object[] { usernameInp.text, passwordInp.text });
+
+        loginBtn.interactable = false;
+
+    }
+
+    private void OnLogin(bool success)
+    {
+        if (!success)
+        {
+            Debug.Log("Active");
+            //errorMsgGO.gameObject.SetActive(true);
+        }
+        loginBtn.interactable = true;
+    }
+
+}
